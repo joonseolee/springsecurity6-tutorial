2c494f413c5ab40055970c5a9e22c2458a489dbd
이준서(Joonseo Lee)/Bot솔루션개발팀/SKP <joonseo.lee@sk.com>
2024-04-04 17:58:38 +0900

    first commit

---
+++ src/main/kotlin/com/joonseolee/security/SpringSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.joonseolee.security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springsecurity6TutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<Springsecurity6TutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/joonseolee/security/Springsecurity6TutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joonseolee.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springsecurity6TutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
