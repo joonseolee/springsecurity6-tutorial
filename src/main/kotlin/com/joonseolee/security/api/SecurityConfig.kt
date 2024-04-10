999e4fe20a3ff96892a96c1534ff407673f1ff39
Joonseo Lee <niceweather94@gmail.com>
2024-04-10 15:17:15 +0900

    feat: 사용자 정의 보안 설정하기

---
+++ src/main/kotlin/com/joonseolee/security/api/SecurityConfig.kt
@@ -0,0 +1,38 @@
+package com.joonseolee.security.api
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { it.anyRequest().authenticated() }
+            .formLogin(Customizer.withDefaults())
+
+        return http.build()
+    }
+
+    /**
+     * 해당 빈이 있을경우 application.yml 파일보다 우선순위가 높다
+     */
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user = User
+            .withUsername("user")
+            .password("{noop}1111")
+            .roles("USER").build()
+
+        return InMemoryUserDetailsManager(user)
+    }
+}
