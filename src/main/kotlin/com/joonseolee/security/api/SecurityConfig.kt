17655e50c209b014fa59765c5605807c7fcd7381
이준서(Joonseo Lee)/Bot솔루션개발팀/SKP <joonseo.lee@sk.com>
2024-04-19 13:11:40 +0900

    style: securityConfig 재정리

--- src/main/kotlin/com/joonseolee/security/api/SecurityConfig.kt
+++ src/main/kotlin/com/joonseolee/security/api/SecurityConfig.kt
@@ -2,6 +2,8 @@ package com.joonseolee.security.api
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
@@ -15,25 +17,12 @@ class SecurityConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .authorizeHttpRequests { it.anyRequest().authenticated() }
-            .formLogin {
+            .authorizeHttpRequests {
                 it
-                    .loginPage("/loginPage")
-                    .loginProcessingUrl("/loginProc")
-                    .defaultSuccessUrl("/", true)
-                    .failureUrl("/failed")
-                    .usernameParameter("userId")
-                    .passwordParameter("passwd")
-                    .successHandler { _, response, authentication ->
-                        println("authenticated: $authentication")
-                        response!!.sendRedirect("/home")
-                    }
-                    .failureHandler { _, response, exception ->
-                        println("exception: ${exception.message}")
-                        response.sendRedirect("/login")
-                    }
-                    .permitAll()
+                    .requestMatchers("/").permitAll()
+                    .anyRequest().authenticated()
             }
+            .formLogin(Customizer.withDefaults())
 
         return http.build()
     }
