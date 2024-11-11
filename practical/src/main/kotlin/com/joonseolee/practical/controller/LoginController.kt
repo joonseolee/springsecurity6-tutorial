4cf3fdfd394bc56a8e46e7f47cc4190de85eb94f
joonseolee <niceweather94@gmail.com>
2024-11-11 21:52:04 +0900

    feat: 커스텀 로그아웃

--- practical/src/main/kotlin/com/joonseolee/practical/controller/LoginController.kt
+++ practical/src/main/kotlin/com/joonseolee/practical/controller/LoginController.kt
@@ -1,5 +1,13 @@
 package com.joonseolee.practical.controller
 
+import com.joonseolee.practical.domain.dto.AccountContext
+import com.joonseolee.practical.domain.dto.AccountDto
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
@@ -15,5 +23,13 @@ class LoginController {
     @GetMapping(value = ["/signup"])
     fun signup(): String {
         return "login/signup"
+    }
+
+    @GetMapping(value = ["/logout"])
+    fun logout(request: HttpServletRequest,
+               response: HttpServletResponse): String {
+        val authentication = SecurityContextHolder.getContextHolderStrategy().context.authentication
+        SecurityContextLogoutHandler().logout(request, response, authentication)
+        return "/"
     }
 }
